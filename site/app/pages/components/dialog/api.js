26337b3f69458c7b4f88309fe48c0803959d270d
Yuhang <abeyuhang@gmail.com>
2019-12-11 16:26:36 +0800

    update dialog translate

--- site/app/pages/components/dialog/api.js
+++ site/app/pages/components/dialog/api.js
@@ -11,51 +11,51 @@ export default () => VM({
         {
           name: 'active',
           type: 'Boolean',
-          description: 'Whether to show/hide the dialog.',
+          description: _t('控制对话框的打开和关闭'),
           defaults: 'false'
         },
         {
           name: 'backdrop',
           type: 'Boolean',
-          description: 'Enable/disable the dialog overlay',
+          description: _t('是否启用叠加的灰色蒙层'),
           defaults: 'true'
         },
         {
           name: 'closeOnEsc',
           type: 'Boolean',
-          description: 'By the default the dialog will close when pressing esc. To disabled that, just set this prop as false',
+          description: _t('默认情况下，按 esc 时对话框将关闭。设置为 false 可取消此行为。'),
           defaults: 'true'
         },
         {
           name: 'closeOnOutsideClick',
           type: 'Boolean',
-          description: 'By the default the dialog will close when clicking outsite. To disabled that, just set this prop as false',
+          description: _t('默认情况下，单击外部时该对话框将关闭。设置为 false 可取消此行为。'),
           defaults: 'true'
         },
         {
           name: 'fullscreen',
           type: 'Boolean',
-          description: 'The dialog will become fullscreen on mobile screens. This option can disable this behavior.',
+          description: _t('控制该对话框将在移动屏幕上是否会变为全屏。'),
           defaults: 'true'
         }
       ]
     },
     events: {
-      headings: ['Name', 'Description', 'Value'],
+      headings: [_t('名称'), _t('描述'), _t('参数')],
       props: [
         {
           name: 'open',
-          description: 'Triggered when a dialog opens',
+          description: _t('对话框打开时触发'),
           value: 'null'
         },
         {
           name: 'close',
-          description: 'Triggered when a dialog closes',
+          description: _t('对话框关闭时触发'),
           value: 'null'
         },
         {
           name: 'click-overlay',
-          description: 'Triggered when clicked outside overlay',
+          description: _t('在叠加层外点击时触发'),
           value: 'null'
         }
       ]
@@ -63,76 +63,76 @@ export default () => VM({
   },
   alert: {
     props: {
-      headings: ['Name', 'Description', 'Default'],
+      headings: [_t('名称'), _t('描述'), _t('默认值')],
       props: [
         {
           name: 'title',
           type: 'String',
-          description: 'The alert title. Optional.',
+          description: _t('通知对话框的标题'),
           defaults: 'null'
         },
         {
           name: 'content',
           type: 'String',
-          description: 'The alert content. Accepts HTML. Optional.',
+          description: _t('通知对话框的内容。支持 html。'),
           defaults: 'null'
         },
         {
           name: 'confirmText',
           type: 'String',
-          description: 'The text inside confirm button',
-          defaults: 'Ok'
+          description: _t('确认按钮的文案'),
+          defaults: _t('确认')
         }
       ]
     }
   },
   confirm: {
     props: {
-      headings: ['Name', 'Description', 'Default'],
+      headings: [_t('名称'), _t('描述'), _t('默认值')],
       props: [
         {
           name: 'title',
           type: 'String',
-          description: 'The alert title. Optional.',
+          description: _t('确认对话框的标题'),
           defaults: 'null'
         },
         {
           name: 'content',
           type: 'String',
-          description: 'The alert content. Accepts HTML. Optional.',
+          description: _t('确认对话框的内容。支持 html。'),
           defaults: 'null'
         },
         {
           name: 'confirmText',
           type: 'String',
-          description: 'The text inside confirm button',
-          defaults: 'Ok'
+          description: _t('确认按钮的文案'),
+          defaults: _t('确认')
         },
         {
           name: 'cancelText',
           type: 'String',
-          description: 'The text inside cancel button',
-          defaults: 'Cancel'
+          description: _t('取消按钮的文案'),
+          defaults: _t('取消')
         },
         {
           name: 'confirmSpinner',
           type: 'Boolean',
-          description: 'Confirm 按钮是否处于加载状态。当该参数为 true 时，Confirm 按钮会展示 spinner 图标并处于 disabled 状态。',
+          description: _t('确认按钮是否处于加载状态。当该参数为 true 时，确认按钮会展示 md-spinner，并处于 disabled 状态。'),
           defaults: 'false'
         }
       ]
     },
     events: {
-      headings: ['Name', 'Description', 'Value'],
+      headings: [_t('名称'), _t('描述'), _t('参数')],
       props: [
         {
           name: 'confirm',
-          description: 'Triggered when the confirm button receives a click',
+          description: _t('点击确认按钮时触发'),
           value: 'null'
         },
         {
           name: 'cancel',
-          description: 'Triggered when the user dismiss the dialog',
+          description: _t('点击取消按钮时触发'),
           value: 'null'
         }
       ]
@@ -140,75 +140,69 @@ export default () => VM({
   },
   prompt: {
     props: {
-      headings: ['Name', 'Description', 'Default'],
+      headings: [_t('名称'), _t('描述'), _t('默认值')],
       props: [
         {
           name: 'defaultValue',
           type: 'Any',
-          description: 'The default input prompt value',
+          description: _t('输入框的默认值'),
           defaults: 'null'
         },
         {
-          name: 'inputName',
-          type: 'String',
-          description: 'The input name attribute',
-          defaults: 'null'
-        },
-        {
-          name: 'inputId',
-          type: 'String',
-          description: 'The input id attribute',
-          defaults: 'null'
+          name: 'inputRequired',
+          type: 'Boolean',
+          description: _t('输入框是否必须填写'),
+          defaults: 'false'
         },
         {
           name: 'inputMaxLength',
           type: 'Number',
-          description: 'Enables the character count, based on the given value.',
+          description: _t('可选参数，指定输入框的最大允许字符数'),
           defaults: 'null'
         },
         {
           name: 'inputPlaceholder',
           type: 'Number',
-          description: 'Sets a optional placeholder on input.',
+          description: _t('输入框的占位提示信息'),
           defaults: 'null'
         },
         {
           name: 'title',
           type: 'String',
-          description: 'The alert title. Optional.',
-          defaults: 'null'
-        },
-        {
-          name: 'content',
-          type: 'String',
-          description: 'The alert content. Accepts HTML. Optional.',
+          description: _t('对话框的标题'),
           defaults: 'null'
         },
         {
           name: 'confirmText',
           type: 'String',
-          description: 'The text inside confirm button',
-          defaults: 'Ok'
+          description: _t('确认按钮的文案'),
+          defaults: _t('确认')
         },
         {
           name: 'cancelText',
           type: 'String',
-          description: 'The text inside cancel button',
-          defaults: 'Cancel'
+          description: _t('取消按钮的文案'),
+          defaults: _t('取消')
+        },
+        {
+          name: 'confirmSpinner',
+          type: 'Boolean',
+          description: _t('确认按钮是否处于加载状态。当该参数为 true 时，确认按钮会展示 md-spinner，并处于 disabled 状态。'),
+          defaults: 'false'
         }
       ]
     },
     events: {
-      headings: ['Name', 'Description', 'Value'],
+      headings: [_t('名称'), _t('描述'), _t('参数')],
       props: [
         {
           name: 'confirm',
-          description: 'Triggered when the confirm button receives a click',
-          value: 'Input Value'
+          description: _t('点击确认按钮时触发'),
+          value: 'inputValue'
         },
         {
           name: 'cancel',
-          description: 'Triggered when the user dismiss the dialog',
+          description: _t('点击取消按钮时触发'),
           value: 'null'
         }
       ]
